154d32f453f138abd9faa4c84f36e322a32229ba
Joe-noh <goflb.jh@gmail.com>
2021-08-22 00:45:45 +0900

    mock tauri function call

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('@tauri-apps/api/tauri', () => {
+  return {
+    invoke: () => {},
+  }
+})
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/tauri/i);
